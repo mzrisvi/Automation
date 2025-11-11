8f084f9c16923e2237fedad6909066b12b53c2ab
mzrisvi <mzrisvi@gmail.com>
2025-11-11 14:49:32 +0800

    Maven project

--- src/test/java/test/Browser.java
+++ src/test/java/test/Browser.java
@@ -1,14 +1,22 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Browser {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         WebDriver driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -17,10 +25,34 @@ public class Browser {
 
         driver.get("https://google.com");
         String original = driver.getWindowHandle();
-        driver.navigate().to("https://automationstepbystep.com/");
+        //driver.navigate().to("https://automationstepbystep.com/");
 
         System.out.println(driver.getCurrentUrl());
         System.out.println(driver.getTitle());
+
+        driver.manage().window().minimize();
+        driver.manage().window().maximize();
+
+        // Screenshots taking
+        File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile, new File("./image1.png"));
+
+        WebElement element = driver.findElement(By.cssSelector(".lnXdpd"));
+        File scrFile1 = element.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(scrFile1, new File("./image2.png"));
+
+        // Create JavascriptExecutor interface object by Type casting
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // Get return value from script
+        WebElement button = driver.findElement(By.name("btnI"));
+        String text = (String) js.executeScript("return arguments[0].innerText", button);
+
+        // JavaScript to click element
+        js.executeScript("arguments[0].click();", button);
+
+        // Execute JS directly
+        js.executeScript("console.log('Hello World')");
 
         driver.navigate().back();
         Thread.sleep(2000);
@@ -31,8 +63,7 @@ public class Browser {
         driver.switchTo().newWindow(WindowType.WINDOW);
         driver.switchTo().newWindow(WindowType.TAB);
 
-       // driver.close();
-
+        // driver.close();
     }
 
 }
--- src/test/java/test/FirstTest.java
+++ src/test/java/test/FirstTest.java
@@ -1,4 +1,33 @@
 package test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FirstTest {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.getTitle();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        message.getText();
+
+        driver.quit();
+    }
 }
