810af26fb51939c277e9ec82abadc62b128557ef
Mohammad Risvi <mzrisvi@gmail.com>
2025-11-16 16:23:32 +0800

    python project add

---
+++ Python/.vscode/HelloPython.py
@@ -0,0 +1,10 @@
+import sys
+
+
+print("Hello, Python!")
+print('Welcome to Visual Studio Code.')
+print("This is a simple Python script.")
+print("Enjoy coding!")
+print("Have a great day!")
+print("Goodbye!")
+print("This is the end of the script.")
