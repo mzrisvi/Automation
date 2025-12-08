75a8f55f2c98e553bdaec4fb4aba03afbdb11bc1
Mohammad Risvi <mzrisvi@gmail.com>
2025-12-08 17:23:23 +0800

    Requirement.txt add

--- API/Playwright_API/Scripts/activate.fish
+++ API/Playwright_API/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Automation\APIs\Playwright_API'
+set -gx VIRTUAL_ENV 'C:\Automation\API\Playwright_API'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
