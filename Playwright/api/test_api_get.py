277774112b1e67d66635e2a7db358f0b6815f75f
Mohammad Risvi <mzrisvi@gmail.com>
2025-12-09 14:06:17 +0800

    update api test

--- Playwright/api/test_api_get.py
+++ Playwright/api/test_api_get.py
@@ -1,11 +1,18 @@
-def test_api_get(playwright):
-        request = playwright.request.new_context()
-        response = request.get("https://jsonplaceholder.typicode.com/posts/1")
+# This is a sample test script using Playwright to perform an API GET request
+import pytest
+
+@pytest.fixture(scope="session")
+def api_context(playwright):
+    context = playwright.request.new_context(ignore_https_errors=True)
+    yield context
+    context.dispose()
+
+def test_api_get(api_context):
+        response = api_context.get("https://jsonplaceholder.typicode.com/posts/1")
 
         assert response.status == 200
         json_response = response.json()
         print("Json Response is :\n",json_response)
         assert json_response["id"] == 1
 
-        request.dispose()
         print("API GET request test completed successfully.")
